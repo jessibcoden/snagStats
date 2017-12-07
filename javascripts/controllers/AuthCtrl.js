651e31d5491e60d98f19c6437c381be1c3974cef
Jess Brawner <jessnbillbrawner@gmail.com>
2017-12-07 10:22:13 -0600

    added navbar with logout functionality, completed auth for new users

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,9 +1,9 @@
 'use strict';
 
-app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthService){
+app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthService, TrackerService){
   $scope.authenticate = () => {
     AuthService.authenticateGoogle().then((result) =>{
-      $rootScope.navbar = true;
+      $rootScope.userLoggedIn = true;
       $scope.$apply(() =>{
         $location.path("/teamDashboard");
       });
@@ -11,4 +11,35 @@ app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthService){
       console.log("error in authenticateGoogle", err);
     });
   };
+
+  $scope.newCoachAuthenticate = () => {
+    AuthService.authenticateGoogle().then((result) =>{
+      $rootScope.userLoggedIn = true;
+      result.user.isCoach = true;
+      result.user.teamId = "";
+      const newTracker = TrackerService.createTrackerObject(result.user);
+      TrackerService.postNewTracker(newTracker).then((result) => {
+        $location.path("/teams/new");
+      });
+    }).catch((err) =>{
+      console.log("error in authenticateGoogle", err);
+    });
+  };
+
+  $scope.newTrackerAuthenticate = () => {
+    AuthService.authenticateGoogle().then((result) =>{
+      $rootScope.userLoggedIn = true;
+      result.user.isCoach = false;
+      result.user.teamId = "";
+      const newTracker = TrackerService.createTrackerObject(result.user);
+      TrackerService.postNewTracker(newTracker).then((result) => {
+        console.log('result', result);
+        $location.path(`/trackers/${result.data.name}/edit`);
+      });
+    }).catch((err) =>{
+      console.log("error in authenticateGoogle", err);
+    });
+  };
+
+
 });
