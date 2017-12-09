34cacbe6a3ba4b1f26cec94582d9a5e02eca8420
Jess Brawner <jessnbillbrawner@gmail.com>
2017-12-09 12:49:26 -0600

    form now clears to add another game

--- javascripts/controllers/GameEditCtrl.js
+++ javascripts/controllers/GameEditCtrl.js
@@ -6,24 +6,17 @@ app.controller("GameEditCtrl", function($location, $routeParams, $scope, AuthSer
 
     $scope.saveAndAddAnotherGame = (game) => {
         let newGame = GameService.createNewGameObject(game, $routeParams.teamId);
-        console.log('newGame', newGame);
-        
-        
             GameService.postNewGame(newGame).then((result) => {
-                $scope.games = result;
+                $scope.game = {};
             }).catch((err) => {
                 console.log("error in saveAndAddAnotherGame", err);
                 });
-            $location.url("/games/new");
-
-        $scope.project = null;
     };
 
     $scope.saveAndClose = (game) => {
         const user = AuthService.getCurrentUid();
         let newGame = GameService.createNewGameObject(game, user);
         GameService.postNewGame(newGame).then((result) => {
-            $scope.games = result;
             $location.url("/team/dashboard");
         }).catch((err) => {
         console.log("error in saveAndClose", err);
